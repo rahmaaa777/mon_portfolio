dad9d2f7d1f871c9ac6c12786969181d90b8780e
rahmaaa777 <ghizlenerahma36@gmail.com>
2025-04-29 22:50:58 +0100

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,6 @@
+// Menu responsive
+console.log("Bienvenue dans mon portfolio !");
+document.getElementById("menu-icon").addEventListener("click", function () {
+    document.getElementById("nav-links").classList.toggle("show");
+
+  });
